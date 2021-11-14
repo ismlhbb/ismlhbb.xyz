908fd334d2873e192ed2b81071e80e206d9c04f9
Ismail Habibi Herman <ismail.habibi2104@gmail.com>
2021-11-14 21:40:17 +0700

    feat: disable `redux`

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import type { AppProps } from 'next/app';
 import { DefaultSeo } from 'next-seo';
-import { Provider } from 'react-redux';
 
 import 'styles/globals.scss';
 import 'styles/SidebarNav.scss';
@@ -11,18 +10,15 @@ import 'styles/Footer.scss';
 import 'styles/Projects.scss';
 import 'react-typist/dist/Typist.css';
 
-import { store } from 'app/store';
 import PageLoadingBar from 'components/PageLoadingBar';
 import { defaultSEOConfig } from 'constants/next-seo';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Provider store={store}>
-        <DefaultSeo {...defaultSEOConfig} />
-        <Component {...pageProps} />
-        <PageLoadingBar />
-      </Provider>
+      <DefaultSeo {...defaultSEOConfig} />
+      <Component {...pageProps} />
+      <PageLoadingBar />
     </>
   );
 }
