2c03e8e1f5f09f1710686b3fb56849ad56a5fd07
Ismail Habibi Herman <ismail.habibi2104@gmail.com>
2024-06-18 22:19:35 +0700

    feat(umami): update trackEvent helper

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -5,14 +5,64 @@ enum EventType {
 }
 
 type TrackEvent = (
-  event_value: string,
+  event_name: string,
   event_type: keyof typeof EventType,
-  url?: string | undefined,
-  website_id?: string | undefined
+  event_data?: { [key: string]: any },
+  website_id?: string
 ) => void;
 
-export const trackEvent: TrackEvent = (...args) => {
-  if (window.umami && typeof window.umami.trackEvent === 'function') {
-    window.umami.trackEvent(...args);
+interface EventData {
+  [key: string]: any;
+  event_type: keyof typeof EventType;
+}
+
+interface CustomPayload {
+  website?: string;
+  event_name: string;
+  event_data: EventData;
+}
+
+interface Umami {
+  track(
+    event_name: string,
+    event_data?: EventData
+  ): Promise<string> | undefined;
+  track(custom_payload: CustomPayload): Promise<string> | undefined;
+  track(
+    callback: (props: {
+      hostname: string;
+      language: string;
+      referrer: string;
+      screen: string;
+      title: string;
+      url: string;
+      website: string;
+    }) => CustomPayload
+  ): Promise<string> | undefined;
+}
+
+declare global {
+  interface Window {
+    umami: Umami;
+  }
+}
+
+export const trackEvent: TrackEvent = (
+  event_name,
+  event_type,
+  event_data = {},
+  website_id
+) => {
+  if (window.umami && typeof window.umami.track === 'function') {
+    const payload: CustomPayload = {
+      event_name,
+      event_data: { ...event_data, event_type },
+    };
+
+    if (website_id) {
+      payload.website = website_id;
+    }
+
+    window.umami.track(payload);
   }
 };
